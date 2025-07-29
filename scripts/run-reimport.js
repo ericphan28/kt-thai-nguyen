96852d5c920e92076dab727092c2df406b3f9beb
Thang Phan <ericphan28@gmail.com>
2025-07-29 21:04:04 +0700

    Xóa script import và thêm next.config.mjs để fix lỗi build Vercel

--- scripts/run-reimport.js
+++
@@ -1,8 +0,0 @@
-const { config } = require('dotenv');
-const path = require('path');
-
-// Load .env.local file
-config({ path: path.resolve(__dirname, '../.env.local') });
-
-// Import and run the reimport script
-require('./reimport_students.ts');
